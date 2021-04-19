211c6217b6e01d573054a5d080faee37fd1c2549
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-04-19 11:18:12 -0400

    API setup

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 // Connect API
-const URL = `api.openweathermap.org/data/2.5/weather?q=boston,usa&APPID=10d288b61704a5239e53beef7ef992cb&units=imperial`
+const DOMAIN = `api.openweathermap.org/data/2.5/weather?`
+const = API_KEY = `&APPID=10d288b61704a5239e53beef7ef992cb`
+const UNIT = `&units=imperial`
+const URL = `${DOMAIN}q=${inputValue}${API_KEY}${UNIT}`
 
 // Search Functionality
+const searchLocation = async () => {
+  try {
+
+    let inputValue = document.querySelector(`input`).value
+    let response = await axios.get(URL)
+
+
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 // Display Endpoint Data
-
+const showWeatherData = () => {
+  
+}
 // Changing Background Images
 
 // Event Listener
