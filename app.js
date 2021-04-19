8aef718359dac567770a78a541c72c598d021611
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-04-19 10:57:12 -0400

    html structure

---
+++ app.js
@@ -0,0 +1,10 @@
+// Connect API
+const URL = `api.openweathermap.org/data/2.5/weather?q=boston,usa&APPID=10d288b61704a5239e53beef7ef992cb&units=imperial`
+
+// Search Functionality
+
+// Display Endpoint Data
+
+// Changing Background Images
+
+// Event Listener
