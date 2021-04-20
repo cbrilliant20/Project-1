90031e53ed316ca26216ec43999fabebc31ef115
Cameron Brilliant <cameron@Camerons-MacBook-Air.local>
2021-04-20 11:25:06 -0400

    background function

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const searchLocation = async () => {
       )
       showWeatherData(response.data)
       changeBackground(response.data)
-      // console.log(response)
+      console.log(response)
     }
   } catch (error) {
     console.error(error)
@@ -97,24 +97,25 @@ const removeMain = () => {
 let changeBackground = (data) => {
   let backImage = document.querySelector(".body")
   if (`${data.weather[0].main}` === `Clouds`) {
-    backImage.style.background = url(
+    backImage.style.backgroundImage = `url(
       "https://images.unsplash.com/photo-1499346030926-9a72daac6c63?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-    )
-  } else if (`${data.weather[0].main}.value` === `Clear`) {
-    backImage.style.background = url(
+    )`
+  } else if (`${data.weather[0].main}` === `Clear`) {
+    backImage.style.backgroundImage = `url(
       "https://images.unsplash.com/photo-1503453363464-743ee9ce1584?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80"
-    )
-  } else if (`${data.weather[0].main}.value` === `Rain`) {
-    backImage.style.background = url(
+    )`
+  } else if (`${data.weather[0].main}` === `Rain`) {
+    backImage.style.backgroundImage = `url(
       "https://images.unsplash.com/photo-1518803194621-27188ba362c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1953&q=80"
-    )
-  } else if (`${data.weather[0].main}.value` === `Thunder`) {
-    backImage.style.background = url(
+    )`
+  } else if (`${data.weather[0].main}` === `Thunder`) {
+    backImage.style.backgroundImage = `url(
       "https://images.unsplash.com/photo-1594760467013-64ac2b80b7d3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80"
-    )
-  } else if (`${data.weather[0].main}.value` === `Snow`) {
-    backImage.style.background = url(
+    )`
+  } else if (`${data.weather[0].main}` === `Snow`) {
+    backImage.style.backgroundImage = `url(
       "https://images.unsplash.com/photo-1453489629239-2d15752eb271?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2089&q=80"
-    )
+    )`
   }
+  // console.log(data.weather[0].main)
 }
